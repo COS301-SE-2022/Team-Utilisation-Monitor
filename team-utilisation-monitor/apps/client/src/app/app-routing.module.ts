e74b517248775742ce1c3d372c515bde0afb3be1
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-05-12 14:44:25 +0200

    💄 (UI):Update routing

--- team-utilisation-monitor/apps/client/src/app/app-routing.module.ts
+++ team-utilisation-monitor/apps/client/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'example',
+    path: '',
     loadChildren: () =>
       import('@team-utilisation-monitor/client/example/feature').then(
         (x) => x.ClientExampleFeatureModule
       ),
   },
+/*
+  {
+    path: 'Login',
+    loadChildren: () =>
+      import('@team-utilisation-monitor/client/Authentication/feature').then(
+        (x) => x.ClientAuthenticationFeatureModule
+      ),
+  },*/
 ]
   @NgModule({
     imports: [RouterModule.forRoot(routes)],
