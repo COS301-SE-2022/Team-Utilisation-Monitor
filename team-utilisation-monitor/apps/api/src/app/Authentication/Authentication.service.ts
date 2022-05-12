2695c7cf2320ab074d12e2bb15ee2bcf86660c28
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-05-13 00:21:01 +0200

    🚧 (Service Engineer):Added an api mock

---
+++ team-utilisation-monitor/apps/api/src/app/Authentication/Authentication.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { UserModel } from "./User.model";
+
+@Injectable()
+export class AuthenticationService{
+  users:UserModel[];
+
+  addUser(companyName:string,Password:string,email:string)
+  {
+    const user=new UserModel(companyName,Password,email);
+    this.users.push(user);
+    return companyName+" succesfully registered";
+  }
+
+  getUser()
+  {
+    return {'companyName':'Oracle','email':'Oracle@gmail.com'}
+  }
+}
--- team-utilisation-monitor/apps/api/src/app/app.module.ts
+++ team-utilisation-monitor/apps/api/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationController } from './Authentication/Authentication.controller';
+import { AuthenticationModule } from './Authentication/Authentication.module';
+import { AuthenticationService } from './Authentication/Authentication.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthenticationModule],
+  controllers: [AppController,AuthenticationController],
+  providers: [AppService,AuthenticationService],
 })
 export class AppModule {}
