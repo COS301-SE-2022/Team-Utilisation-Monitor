826ce8d977ba092dcbabcd4ef0812c2f082d3e38
Agape <theaman249@gmail.com>
2022-05-21 17:13:16 +0200

    🚧 (prismaService) Added a prisma service

--- team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.ts
+++ team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import {UserPerson} from '@team-utilisation-monitor/api/shared/data-access'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class DataAccessRepository {
 
+    constructor(private readonly prisma:PrismaClient ){}
     
-
 }
