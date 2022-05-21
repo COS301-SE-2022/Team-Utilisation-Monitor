6bf0560ee1589a6db71433b5674b8a8b7d24e3c8
Rourke9001 <92760271+Rourke9001@users.noreply.github.com>
2022-05-21 19:15:06 +0200

    Added GraphQL

--- team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.spec.ts
+++ team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.spec.ts
@@ -4,11 +4,10 @@ import { ApiFeatureResolver } from './api-feature.resolver';
 describe('ApiFeatureResolver', () => {
   let resolver: ApiFeatureResolver;
 
-  beforeEach( async () => {
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ApiFeatureResolver],
-    })
-      .compile();
+    }).compile();
 
     resolver = module.get<ApiFeatureResolver>(ApiFeatureResolver);
   });
