7d4e8b6fcde8ac7c887668ccfc2049040ca16d7c
Agape <theaman249@gmail.com>
2022-05-21 15:08:35 +0200

    🚧 (Api)Added resolver.ts file

---
+++ team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ApiFeatureResolver } from './api-feature.resolver';
+
+describe('ApiFeatureResolver', () => {
+  let resolver: ApiFeatureResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ApiFeatureResolver],
+    }).compile();
+
+    resolver = module.get<ApiFeatureResolver>(ApiFeatureResolver);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+});
