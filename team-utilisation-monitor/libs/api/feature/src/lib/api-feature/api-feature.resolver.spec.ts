625cbc3d05f833a35f9a763876e2ddd311109814
Rourke9001 <92760271+Rourke9001@users.noreply.github.com>
2022-05-21 17:17:53 +0200

    TODO: Add Mock Data

--- team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.spec.ts
+++ team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.spec.ts
@@ -4,10 +4,11 @@ import { ApiFeatureResolver } from './api-feature.resolver';
 describe('ApiFeatureResolver', () => {
   let resolver: ApiFeatureResolver;
 
-  beforeEach(async () => {
+  beforeEach( async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ApiFeatureResolver],
-    }).compile();
+    })
+      .compile();
 
     resolver = module.get<ApiFeatureResolver>(ApiFeatureResolver);
   });
