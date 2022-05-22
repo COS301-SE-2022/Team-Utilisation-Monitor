406a79ba81091e41197e63f61ebb7d2cfe525403
Agape <theaman249@gmail.com>
2022-05-22 13:32:05 +0200

    🚧 (api) api layer is complete

--- team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.ts
+++ team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.ts
@@ -1,4 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
 
-@Resolver()
-export class ApiFeatureResolver {}
+import { Query, Resolver } from '@nestjs/graphql';
+import { UserPerson } from '@team-utilisation-monitor/api/shared/data-access';
+import {ServiceFeatureService} from '@team-utilisation-monitor/service/feature'
+
+
+@Resolver(()=>UserPerson)
+export class ApiFeatureResolver {
+
+    constructor(private readonly service:ServiceFeatureService ){}
+
+    @Query(()=>UserPerson)
+    async login(email:string,password:string){
+        const resp=this.service.login(email,password);
+
+        return resp;
+    }
+
+    @Query(() => String)
+  sayHello(): string {
+    return 'Hello World!';
+  }
+}
