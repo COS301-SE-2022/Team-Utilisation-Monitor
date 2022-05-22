30429987094848558ab78dbdd23fbb858b802f33
Rourke9001 <92760271+Rourke9001@users.noreply.github.com>
2022-05-22 20:15:57 +0200

    Added deleteUser Mutation

--- team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.ts
+++ team-utilisation-monitor/libs/api/feature/src/lib/api-feature/api-feature.resolver.ts
@@ -1,24 +1,35 @@
 
-import { Query, Resolver, Mutation } from '@nestjs/graphql';
+import { Query, Args, Resolver, Mutation } from '@nestjs/graphql';
 import { UserPerson } from '@team-utilisation-monitor/api/shared/data-access';
 import {ServiceFeatureService} from '@team-utilisation-monitor/service/feature'
 
 import { UserInputError } from 'apollo-server-express';
 
-@Resolver(()=>UserPerson)
+@Resolver(() => UserPerson)
 export class ApiFeatureResolver {
 
-    constructor(private readonly service:ServiceFeatureService ){}
+  constructor(private readonly service: ServiceFeatureService ) {}
 
-    @Query(()=>UserPerson)
-    async login(email:string,password:string){
-        const resp=this.service.login(email,password);
-        return resp;
-    }
+  @Query(() => UserPerson)
+  async login(email:string,password:string){
+      const resp=this.service.login(email,password);
+      return resp;
+  }
   
 
-    @Query(() => String)
-      sayHello(): string {
+  @Query(() => String)
+  sayHello(): string {
     return 'Hello World!';
   }
+
+  @Query(() => String)
+  pingUser() {
+    return 'on';
+  }
+
+  @Mutation((returns) => UserPerson)
+  async deleteUser(@Args('userNum', { type: () => String }) id: string) {
+    return new UserInputError('Not implemented');
+  }
+
 }
