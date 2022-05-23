031c70ca5835db3cccb5a6bf802dd4f96506b34f
Agape <theaman249@gmail.com>
2022-05-23 05:21:16 +0200

    🚀 (marrige)first marriage front and backend

--- team-utilisation-monitor/apps/client/src/app/app.module.ts
+++ team-utilisation-monitor/apps/client/src/app/app.module.ts
@@ -6,9 +6,13 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,],
+  imports: [FormsModule,HttpClientModule,BrowserModule, AppRoutingModule, BrowserAnimationsModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
