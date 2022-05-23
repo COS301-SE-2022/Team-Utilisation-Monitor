b4f280ccc40d830d02949e4d134e43f7ab7b5cf9
Agape <theaman249@gmail.com>
2022-05-24 01:43:59 +0200

    🚀 (sign-up)signup functionality is done front-end

--- team-utilisation-monitor/libs/client/authentication/feature/src/lib/client-authentication-service/client-authentication-service.service.ts
+++ team-utilisation-monitor/libs/client/authentication/feature/src/lib/client-authentication-service/client-authentication-service.service.ts
@@ -8,26 +8,67 @@ import { HttpHeaders } from '@angular/common/http';
 })
 export class ClientAuthenticationServiceService {
 
-    constructor(private readonly httpClient:HttpClient){}
+  constructor(private readonly httpClient:HttpClient){}
 
-    login(email:string,password:string):Observable<any>{
-        const query='query{login(email:"'+email+'",password:"'+password+'"){name}}';
+  login(email:string,password:string):Observable<any>
+  {
+    const query='query{login(email:"'+email+'",password:"'+password+'"){name}}';
 
-        const options = {
-            headers: new HttpHeaders({
-              'Content-Type': 'application/json'
-            })
-        }
-
-        const obj= this.httpClient.post<any>('http://localhost:3333/graphql',JSON.stringify({ query: query }), options);
-        
-        console.log(obj);
-
-        return obj;
-
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+        })
     }
+
+    const obj= this.httpClient.post<any>('http://localhost:3333/graphql',JSON.stringify({ query: query }), options);
+        
+    //console.log(obj);
+
+    return obj;
+  }
+
+  signup(name:string,surname:string,email:string,role:string,suspended:string,password:string,company_name:string):Observable<any>
+  {
+
+    console.log("In SignUP-Service");
+
+    const query='mutation{createPerson(name:"'+name+'",surname:"'+surname+'",email:"'+email+'",role:"'+role+'",suspended:"'+suspended+'",password:"'+password+'",company_name:"'+company_name+'"){name surname email role password suspended company_name}}';
+
+    console.log(query);
+
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+        })
+    }
+
+    const obj= this.httpClient.post<any>('http://localhost:3333/graphql',JSON.stringify({ query: query }), options);
+        
+    console.log(obj);
+
+    return obj;
+
+  }
     
 }
+
+/***
+ * mutation{
+  createPerson(name:"Gift",surname:"Monwa",email:"G@gmail.com",
+  role:"USER",suspended:"false",password:"CodeCs",company_name:"theSoftCompany")
+  {
+    name
+    surname
+    email
+    password
+    role
+    suspended
+    company_name
+  }
+}
+ */
+
+
 
 
 /***
--- team-utilisation-monitor/libs/client/authentication/feature/src/lib/example/example.component.ts
+++ team-utilisation-monitor/libs/client/authentication/feature/src/lib/example/example.component.ts
@@ -19,6 +19,23 @@ export class ExampleComponent implements OnInit {
     password: new FormControl('')
   })
 
+  /**
+   *  createPerson(name:"Gerald",surname:"Clayton",email:"gerald@gmail.com",
+  role:"ADMIN",suspended:"false",password:"myIdeal",company_name:"theSoftCompany")
+  
+   */
+
+  signUpForm=new FormGroup({
+    name: new FormControl(''),
+    surname:new FormControl(''),
+    email:new FormControl(''),
+    role:new FormControl(''),
+    suspended:new FormControl(''),
+    password:new FormControl(''),
+    company_name:new FormControl(''),
+
+  })
+
 
 
   onSubmit(formdata: { email: string; password: string; })
@@ -32,6 +49,27 @@ export class ExampleComponent implements OnInit {
             //localStorage.setItem("id", item.data.login.id);
 
             console.log("logged In!!!");
+            console.log(item);
+
+            //this._router.navigate(['CompanyRepresentativeHome'], {state: {id: item.data.login.id}, queryParamsHandling: "preserve"});
+          }else{
+            alert("Incorrect Details, Try Again!");
+          }
+        },
+      error: (err) => { console.log(err); }
+      });
+    }
+  }
+
+  signUp(formdata:{name:string,surname:string,email:string,role:string,suspended:string,password:string,company_name:string})
+  {
+    if(this.profileForm.valid) {
+      this.result = this.API.signup(formdata.name,formdata.surname,formdata.email,formdata.role,formdata.suspended,formdata.password,formdata.company_name).subscribe({
+        next: (item) => {
+          if (item.data != null){
+            //localStorage.setItem("id", item.data.login.id);
+
+            console.log("Signed  In!");
             console.log(item);
 
             //this._router.navigate(['CompanyRepresentativeHome'], {state: {id: item.data.login.id}, queryParamsHandling: "preserve"});
