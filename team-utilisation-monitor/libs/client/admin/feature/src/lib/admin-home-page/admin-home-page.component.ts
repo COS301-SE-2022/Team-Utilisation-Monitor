137292ca0f15acf832bc0bfe574550ad3072750f
Cornel Coetzee <94779890+L3m0nRoCk@users.noreply.github.com>
2022-05-28 00:23:55 +0200

    Updated Admin Front End
    Added Multiple Documents:
    - added HTML and CSS for Admin Home, Company and List view.
    - updated SideNav to include navigation between pages
    - added Card items for Admin Company View
    - Added Remove Button to remove individuals from the company

--- team-utilisation-monitor/libs/client/admin/feature/src/lib/admin-home-page/admin-home-page.component.ts
+++ team-utilisation-monitor/libs/client/admin/feature/src/lib/admin-home-page/admin-home-page.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminHomePageComponent implements OnInit {
   //constructor() {}
-  
+  events: string[] = [];
+  opened = true;
+
+
   ngOnInit(): void {
     console.log();
   }
