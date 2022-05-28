5f1b5bb9f209e8f99943d5070bf1acd3b815fc71
Rourke9001 <92760271+Rourke9001@users.noreply.github.com>
2022-05-28 12:03:36 +0200

    Added Get User ID

---
+++ team-utilisation-monitor/libs/service/feature/src/lib/queries/handlers/get-user-id.handler.ts
@@ -0,0 +1,13 @@
+import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
+import { DataAccessRepository } from "@team-utilisation-monitor/repository/data-access";
+import { GetUserIDQuery } from "../impl/get-user-id.query";
+
+// @QueryHandler(GetOnePersonQuery)
+// export class GetOnePersonQueryHandler implements IQueryHandler<GetOnePersonQuery>{
+
+//     constructor(private readonly repository:DataAccessRepository){}
+
+//     async execute(query: GetOnePersonQuery): Promise<any> {
+//         return this.repository.getOnePersonVEmail(query.email);
+//     }
+// }
