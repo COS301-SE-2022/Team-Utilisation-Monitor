e9fa4e65398d6c0bcccab43fbdf9516237357329
Rourke9001 <92760271+Rourke9001@users.noreply.github.com>
2022-05-29 20:45:48 +0200

    Fixed Errors in Get User ID Handler

--- team-utilisation-monitor/libs/service/feature/src/lib/queries/handlers/get-user-id.handler.ts
+++ team-utilisation-monitor/libs/service/feature/src/lib/queries/handlers/get-user-id.handler.ts
@@ -2,22 +2,12 @@ import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { DataAccessRepository } from "@team-utilisation-monitor/repository/data-access";
 import { GetUserIDQuery } from "../impl/get-user-id.query";
 
-// @QueryHandler(GetOnePersonQuery)
-// export class GetOnePersonQueryHandler implements IQueryHandler<GetOnePersonQuery>{
-
-//     constructor(private readonly repository:DataAccessRepository){}
-
-//     async execute(query: GetOnePersonQuery): Promise<any> {
-//         return this.repository.getOnePersonVEmail(query.email);
-//     }
-// }
-
 @QueryHandler(GetUserIDQuery)
-export class GetOnePersonQueryHandler implements IQueryHandler<GetUserIDQuery>{
+export class GetUserIDQueryHandler implements IQueryHandler<GetUserIDQuery>{
 
     constructor(private readonly repository:DataAccessRepository){}
 
     async execute(query: GetUserIDQuery): Promise<any> {
-        return this.repository.getOnePersonVEmail(query.user);
+        return this.repository.getUserID(query.email);
     }
 }
