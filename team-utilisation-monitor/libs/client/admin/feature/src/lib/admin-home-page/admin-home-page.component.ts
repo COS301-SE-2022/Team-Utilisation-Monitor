58834a9842adb56b0e0d374147b1b2790672a160
Cornel Coetzee <94779890+L3m0nRoCk@users.noreply.github.com>
2022-05-30 01:20:20 +0200

    Added SideNav component to Admin Home Page
    - Added SideNav component to Admin Home Page

--- team-utilisation-monitor/libs/client/admin/feature/src/lib/admin-home-page/admin-home-page.component.ts
+++ team-utilisation-monitor/libs/client/admin/feature/src/lib/admin-home-page/admin-home-page.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminHomePageComponent implements OnInit {
   //constructor() {}
-  events: string[] = [];
-  opened = true;
+  boolshow = true;
 
 
   ngOnInit(): void {
