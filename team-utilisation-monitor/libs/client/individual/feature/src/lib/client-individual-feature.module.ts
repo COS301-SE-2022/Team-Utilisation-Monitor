25b57c6574d12682030e0e253501a1e909966186
thangeniF <faresatheo1@gmail.com>
2022-05-30 18:48:30 +0200

    🚧(frontend)- individual shell, cornel's styling

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/client-individual-feature.module.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/client-individual-feature.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { IndividualHomePageComponent } from './individual-home-page/individual-home-page.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   imports: [
@@ -10,6 +14,10 @@ import { IndividualHomePageComponent } from './individual-home-page/individual-h
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
   ],
   declarations: [
     IndividualHomePageComponent
--- team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-home-page/individual-home-page.component.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-home-page/individual-home-page.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndividualHomePageComponent implements OnInit {
 
-  constructor() { }
+
+  events: string[] = [];
+  opened = true;
 
   ngOnInit(): void {
+    console.log();
   }
 
 }
