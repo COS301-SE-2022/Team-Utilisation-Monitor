ec6470e14006cb533503bbefa69c40b4ba4aed8e
thangeniF <faresatheo1@gmail.com>
2022-05-30 19:19:00 +0200

    🚧(frontend)- individual shell, cornel's styling

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/client-individual-feature.module.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/client-individual-feature.module.ts
@@ -6,6 +6,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { Router } from '@angular/router';
 
 @NgModule({
   imports: [
