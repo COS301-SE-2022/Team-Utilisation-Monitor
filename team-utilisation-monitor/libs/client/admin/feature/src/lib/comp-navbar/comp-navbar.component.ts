aed6573bb15cc20811db443d50ca150077641643
Cornel Coetzee <94779890+L3m0nRoCk@users.noreply.github.com>
2022-05-31 23:56:30 +0200

    Side/Top Nav backend link
    - Added link to ts file for adminName in Navbar
    - Added link to ts file for companyName in TopNav

--- team-utilisation-monitor/libs/client/admin/feature/src/lib/comp-navbar/comp-navbar.component.ts
+++ team-utilisation-monitor/libs/client/admin/feature/src/lib/comp-navbar/comp-navbar.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompNavbarComponent implements OnInit {
   //constructor() {} 
-  
+  adminName = "Agape Mamphasa";
+
+
   ngOnInit(): void {
     console.log()
   }
