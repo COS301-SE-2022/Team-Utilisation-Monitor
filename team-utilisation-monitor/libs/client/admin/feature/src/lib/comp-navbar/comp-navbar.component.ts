2bec57bbbd3fc0469c847ef80f4b2def2f79264e
Cornel Coetzee <94779890+L3m0nRoCk@users.noreply.github.com>
2022-06-01 00:11:48 +0200

    Added Requested users drop down to SideNav
    - Added Requested users drop down to sidenav
    - Added a "Add User" button to the dropdown

--- team-utilisation-monitor/libs/client/admin/feature/src/lib/comp-navbar/comp-navbar.component.ts
+++ team-utilisation-monitor/libs/client/admin/feature/src/lib/comp-navbar/comp-navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CompNavbarComponent implements OnInit {
   //constructor() {} 
   adminName = "Agape Mamphasa";
-
+  requestOpenState = true;
 
   ngOnInit(): void {
     console.log()
