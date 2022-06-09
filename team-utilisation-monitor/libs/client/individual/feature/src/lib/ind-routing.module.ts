c41db524dbf66370921151a076abad345c20184d
thangeniF <faresatheo1@gmail.com>
2022-06-09 17:35:20 +0200

    🚧(front-end)- fixing individual profile oage routing

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/ind-routing.module.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/ind-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {IndividualHomePageComponent} from "./individual-home-page/individual-home-page.component";
 import {ClientIndividualServiceService} from "./client-individual-service/client-individual-service.service";
-//import {IndividualProfileComponent} from "./individual-profile/individual-profile.component";
+import {IndividualProfileComponent} from "./individual-profile/individual-profile.component";
 
 const routes: Routes = [
   {
@@ -13,10 +13,10 @@ const routes: Routes = [
     path: 'individual_home_page',
     component: IndividualHomePageComponent,
   },
-  /*{
+  {
     path: 'individual_profile_page',
     component: IndividualProfileComponent,
-  },*/
+  },
   {
     path: '**',
     redirectTo: 'login_page',
