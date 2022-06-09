97bbe33d3d90f3cf29e13d429daddb12765dc91a
Cornel Coetzee <94779890+L3m0nRoCk@users.noreply.github.com>
2022-06-09 17:35:54 +0200

    Create Team/Project View page.
    - added initial HTML/CSS
    - Need to add components for team and project

--- team-utilisation-monitor/libs/client/admin/feature/src/lib/admin-team-project-view/admin-team-project-view.component.ts
+++ team-utilisation-monitor/libs/client/admin/feature/src/lib/admin-team-project-view/admin-team-project-view.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-team-project-view.component.scss'],
 })
 export class AdminTeamProjectViewComponent implements OnInit {
-  constructor() {}
+  //constructor() {}
+  boolshow = true;
+  panelOpenState = false;
+  
+  OutTeamNames = [{Name: "Cornel Coetzee"},
+                  {Name: "Gift Monwa"},
+                  {Name: "Rourke Amiss"},
+                  {Name: "Agape Mamphasa"},
+                ];
 
-  ngOnInit(): void {}
+  OutProjectName = [{Name: "Mr Cornel Coetzee"},
+                    {Name: "Mr Gift Monwa"},
+                    {Name: "Mr Rourke Amiss"},
+                    {Name: "Mr Agape Mamphasa"},
+                    {Name: "Mr Agape Mamphasa"},
+                  ];
+
+  ngOnInit(): void {
+    console.log();
+  }
 }
