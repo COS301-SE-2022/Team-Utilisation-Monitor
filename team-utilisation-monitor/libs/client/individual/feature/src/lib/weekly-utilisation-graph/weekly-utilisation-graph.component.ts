6a752e31ab7e4a02c01af9edd1968b3f709cb253
thangeniF <faresatheo1@gmail.com>
2022-07-13 16:31:09 +0200

    💩(front-end)-utilization graph as an angular component

---
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/weekly-utilisation-graph/weekly-utilisation-graph.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'team-utilisation-monitor-weekly-utilisation-graph',
+  templateUrl: './weekly-utilisation-graph.component.html',
+  styleUrls: ['./weekly-utilisation-graph.component.scss']
+})
+export class WeeklyUtilisationGraphComponent implements OnInit {
+
+  //constructor() { }
+
+  ngOnInit(): void {
+    console.log();
+  }
+
+}
