e212acceffd71daf59222f26759fc0faae93b158
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-07-13 20:21:36 +0200

    API:Created the Add Team mmber Command

---
+++ team-utilisation-monitor/libs/service/feature/src/lib/commands/handlers/addTeamMember.handler.ts
@@ -0,0 +1,15 @@
+import { CommandHandler, IQueryHandler} from "@nestjs/cqrs";
+import { DataAccessRepository } from "@team-utilisation-monitor/repository/data-access";
+import { AddTeamMemberCommand } from "../impl/addTeamMember.command";
+
+@CommandHandler(AddTeamMemberCommand)
+export class AddTeamMemberHandler implements IQueryHandler<AddTeamMemberCommand>{
+
+  constructor(private readonly repository:DataAccessRepository){}
+
+  async execute(command: AddTeamMemberCommand): Promise<any> {
+
+//    return this.repository
+  }
+
+}
