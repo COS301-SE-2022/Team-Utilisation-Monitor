64829ac5a1e387c5a4119a6a886531ec627f71dc
thangeniF <faresatheo1@gmail.com>
2022-07-13 16:39:25 +0200

    💩(front-end)-utilization graph as an angular component

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/weekly-utilisation-graph/weekly-utilisation-graph.component.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/weekly-utilisation-graph/weekly-utilisation-graph.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class WeeklyUtilisationGraphComponent implements OnInit {
 
   //constructor() { }
+  public lineChartLabels = ['Week 1', 'Week2', 'Week 3', 'Week 4'];
+  public lineChartLegend = true;
+
+  public lineChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Weekly Utilization'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Hours Spent'}
+  ];
 
   ngOnInit(): void {
     console.log();
