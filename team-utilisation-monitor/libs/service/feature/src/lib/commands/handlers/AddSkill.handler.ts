00c261eaf3acf0217e03ed60daca417c0c74928f
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-07-18 23:43:33 +0200

    ⚡️ (API):Added the AddSkill Command and Handler

---
+++ team-utilisation-monitor/libs/service/feature/src/lib/commands/handlers/AddSkill.handler.ts
@@ -0,0 +1,13 @@
+import { CommandHandler, IQueryHandler} from "@nestjs/cqrs";
+import { DataAccessRepository } from "@team-utilisation-monitor/repository/data-access";
+import { AddSkillCommand } from "../impl/AddSkill.command";
+
+
+@CommandHandler(AddSkillCommand)
+export class AddSkillHandler implements IQueryHandler<AddSkillCommand>
+{
+  constructor(public repository:DataAccessRepository){}
+  async execute(query: AddSkillCommand): Promise<any> {
+      return this.repository.addSkill(query.skillType);
+  }
+}
