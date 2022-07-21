c49ddbe87cb5dc09ecdf2c6dc5375bb65013c95b
Rourke9001 <92760271+Rourke9001@users.noreply.github.com>
2022-07-21 17:21:36 +0200

    TODO Unit Testing in data-access.repository.spec.ts

--- team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.spec.ts
+++ team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.spec.ts
@@ -1,10 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataAccessRepository } from './data-access.repository';
-
 import { PrismaService } from '@team-utilisation-monitor/shared/services/prisma-services'
 
+import { UserPerson } from '@team-utilisation-monitor/api/shared/data-access'
 describe('DataAccessRepository', () => {
   let controller: DataAccessRepository;
+
+  const mockUserObject = {
+    getOnePersonVEmail: jest.fn(UserPerson => {
+      return {
+        id: 123, 
+        name: "rourke",
+        surname: "amiss",
+        email: "foo@bar.com",
+        ...UserPerson
+      }
+    })
+  };
 
   // beforeEach(async () => {
   //   const module: TestingModule = await Test.createTestingModule({
@@ -15,17 +27,32 @@ describe('DataAccessRepository', () => {
   // });
 
   beforeEach(async () => {
-  const module: TestingModule = await Test.createTestingModule({
-    controllers : [DataAccessRepository],
-    providers: [PrismaService],
-  }).compile();
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [DataAccessRepository],
+      providers: [PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockUserObject)
+      .compile();
 
-  controller = module.get<DataAccessRepository>(DataAccessRepository);
+    controller = module.get<DataAccessRepository>(DataAccessRepository);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
+  it('should return a UserPerson', () => {
+    expect(controller.getOnePersonVEmail('foo@bar.com')).toEqual(UserPerson)
+    
+  })
   
 });
+
+/*
+getOnePersonVEmail(arg_email:string):Promise<UserPerson|string>
+getTeam(team_ID:number):Promise<TeamEntity>
+getCompanyVName(f_company_name:string):Promise<UserCompany|null>
+getEmployeesOfCompany(companyName:string):Promise<UserPerson[]>
+getCompanyVID(f_id:number):Promise<UserCompany|null>
+*/
