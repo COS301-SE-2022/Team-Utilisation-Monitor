eb6f099f771a1a61c812aaae3a1b1ff752e3cda6
thangeniF <faresatheo1@gmail.com>
2022-07-24 18:17:56 +0200

    🧑‍💻 (backend)-form validation needed

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-profile/individual-profile.component.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-profile/individual-profile.component.ts
@@ -94,10 +94,8 @@ export class IndividualProfileComponent implements OnInit {
         {
           if(this.currSkills[v] === this.newSkills[i])
           {
-            const index = this.newSkills.indexOf(this.newSkills[v]);
-            console.log(index);
-
-            this.newSkills.splice(index, 1);
+            const index = this.newSkills.indexOf(this.newSkills[v])
+            this.newSkills.splice(index, 1)
             break
           }
         }
@@ -125,21 +123,21 @@ export class IndividualProfileComponent implements OnInit {
     const last_name=this.profileForm.get("last_name")?.value!;
     const skill_name=this.profileForm.get("skill_name")?.value!;
 
-    if(first_name==="" && last_name !=="")
+    if(first_name=="" && last_name !=="")
     {
       this.service.UpdateProfile(this.email,this.fName,last_name).subscribe(Result=>
       {
         console.log(Result.data)
       })
     }
-    else if(last_name==="" && first_name !=="")
+     if(last_name=="" && first_name !=="")
     {
       this.service.UpdateProfile(this.email,this.fName,this.lastName).subscribe(Result=>
       {
         console.log(Result.data)
       })
     }
-    else if(first_name==="" && last_name===""){
+   if(first_name=="" && last_name==""){
       //call the function for skills only
       this.service.UpdateProfile(this.email,this.fName,this.lastName).subscribe(Result=>
         {
@@ -147,7 +145,7 @@ export class IndividualProfileComponent implements OnInit {
         })
     }
 
-    if( first_name!=="" && last_name!=""){
+    if( first_name!="" && last_name!=""){
       this.service.UpdateProfile(this.email,first_name,last_name).subscribe(Result=>
       {
         console.log(Result.data)
