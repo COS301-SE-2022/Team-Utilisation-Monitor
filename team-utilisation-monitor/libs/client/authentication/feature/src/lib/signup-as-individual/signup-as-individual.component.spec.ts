e93a5406e5ec2aa2311500d02012d2a4c1c00d63
thangeniF <faresatheo1@gmail.com>
2022-07-25 22:43:39 +0200

    🚧 (front-end)-working sign up as individual test

--- team-utilisation-monitor/libs/client/authentication/feature/src/lib/signup-as-individual/signup-as-individual.component.spec.ts
+++ team-utilisation-monitor/libs/client/authentication/feature/src/lib/signup-as-individual/signup-as-individual.component.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClient} from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import {HttpClientTestingModule} from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing';
@@ -12,7 +12,7 @@ describe('SignupAsIndividualComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ SignupAsIndividualComponent ],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule,RouterTestingModule],
       providers: [SignupAsIndividualComponent],
       schemas: [NO_ERRORS_SCHEMA]
     })
