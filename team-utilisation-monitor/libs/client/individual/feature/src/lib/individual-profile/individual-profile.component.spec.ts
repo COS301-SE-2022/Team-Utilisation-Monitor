999cb12a56e3549271453f00532c44cbab62b5e1
thangeniF <faresatheo1@gmail.com>
2022-07-25 21:24:31 +0200

    🚧 (forn-end)-focus test

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-profile/individual-profile.component.spec.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-profile/individual-profile.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import { IndividualProfileComponent } from './individual-profile.component';
 
-describe('IndividualProfileComponent', () => {
+fdescribe('IndividualProfileComponent', () => {
   let component: IndividualProfileComponent;
   let fixture: ComponentFixture<IndividualProfileComponent>;
 
@@ -16,7 +16,6 @@ describe('IndividualProfileComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
 
   it('should create', () => {
     expect(component).toBeTruthy();
