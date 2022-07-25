81adfb2b5493084798ccdb8f6992b9a20e3556d1
thangeniF <faresatheo1@gmail.com>
2022-07-25 19:26:25 +0200

    🚧 (front-end)-removed import

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-profile/individual-profile.component.spec.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-profile/individual-profile.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
 import { IndividualProfileComponent } from './individual-profile.component';
 
 describe('IndividualProfileComponent', () => {
@@ -21,17 +20,7 @@ describe('IndividualProfileComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it("testing title", ()=>{
-    expect(component.companyName).toBe("");
-  });
 
-  it("tests html element", ()=>{
-    const data=fixture.nativeElement;
-    expect(data.querySelector(".some").textContent).toContain("user");
-  });
 
-  it("testing functions", ()=>{
-    
-
-  }
+ 
 });
