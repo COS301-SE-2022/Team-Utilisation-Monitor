8592c7837634b9ad0219929c7a27efd16ffac41c
thangeniF <faresatheo1@gmail.com>
2022-07-26 10:34:55 +0200

    🚧 (front-end)- button and display tests

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-profile/individual-profile.component.spec.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/individual-profile/individual-profile.component.spec.ts
@@ -1,5 +1,5 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { CUSTOM_ELEMENTS_SCHEMA, importProvidersFrom, NO_ERRORS_SCHEMA } from '@angular/core';
+import {  HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IndividualProfileComponent } from './individual-profile.component';
 
@@ -7,22 +7,62 @@ fdescribe('IndividualProfileComponent', () => {
   let component: IndividualProfileComponent;
   let fixture: ComponentFixture<IndividualProfileComponent>;
 
+  class form {
+    // getter properties wait to query the DOM until called.
+    get buttons() {
+      return this.queryAll<HTMLButtonElement>('button');
+    }
+    get saveBtn() {
+      return this.buttons[0];
+    }
+    
+    get nameDisplay() {
+      return this.query<HTMLElement>('span');
+    }
+    get nameInput() {
+      return this.query<HTMLInputElement>('input');
+    }
+  
+    gotoListSpy: jasmine.Spy;
+    navigateSpy: jasmine.Spy;
+  
+    constructor(someFixture: ComponentFixture<IndividualProfileComponent>) {
+      // get the navigate spy from the injected router spy object
+      const routerSpy = someFixture.debugElement.injector.get(Router) as any;
+      this.navigateSpy = routerSpy.navigate;
+  
+      // spy on component's `gotoList()` method
+      const someComponent = someFixture.componentInstance;
+      this.gotoListSpy = spyOn(someComponent, 'gotoList').and.callThrough();
+    }
+  
+    //// query helpers ////
+    private query<T>(selector: string): T {
+      return fixture.nativeElement.querySelector(selector);
+    }
+  
+    private queryAll<T>(selector: string): T[] {
+      return fixture.nativeElement.querySelectorAll(selector);
+    }
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ IndividualProfileComponent ],
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(IndividualProfileComponent);
     component = fixture.componentInstance;
+
+    
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
 
 });
