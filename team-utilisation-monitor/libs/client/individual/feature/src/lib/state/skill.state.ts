ab6087ffc496daa6440e097531959690a9606331
thangeniF <faresatheo1@gmail.com>
2022-08-20 21:42:13 +0200

    (front-end) -skill stste model

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/state/skill.state.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/state/skill.state.ts
@@ -1,12 +1,27 @@
 import{ State, Action, StateContext, Selector} from '@ngxs/store';
 import {Skill} from './../models/skill.model';
 import{ AddSkill} from "../actions/skill.actions";
+import {
+  IncreaseNumberOfEmployees
+} from "../../../../../admin/feature/src/lib/actions/mutate-number-of-employees.action";
+import {IncreaseNumberOfEmployeesStateModel} from "@team-utilisation-monitor/client/admin/feature";
 
 export class SkillStateModel{
-  skill: Skill[]
+  skill: Skill[]=[]
 }
 
 @State<SkillStateModel>({
   name:'skill',
-  defaults:{skill:[] }
+  defaults:{ skill:[], }
 })
+
+export class SkillState{
+  @Selector()
+  static getSkill(state:SkillStateModel){
+    if(state.skill.length==0)
+      return state.skill[0].value;
+    else
+      return state.skill[state.skill.length-1].value;
+  }
+
+}
