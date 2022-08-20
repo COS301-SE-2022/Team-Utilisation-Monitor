2b0afa013aae0d51c40d7c85f16527b5fc43cd5f
thangeniF <faresatheo1@gmail.com>
2022-08-20 21:17:14 +0200

    🚧(front-end)-individual skill constructor

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/actions/skill.actions.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/actions/skill.actions.ts
@@ -1,8 +1,9 @@
-import {Skill} from './../models/skill.model'
+import {Skill} from '../models/skill.model'
 
 export class AddSkill{
 
   static readonly type= '[Skill] add'
 
+  constructor(public skill: Skill){}
 
 }
