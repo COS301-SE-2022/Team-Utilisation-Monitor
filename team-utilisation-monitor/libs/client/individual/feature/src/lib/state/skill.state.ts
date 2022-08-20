c532e0492e417c9b1cfcafe397cf4915736f1fcd
thangeniF <faresatheo1@gmail.com>
2022-08-20 21:47:23 +0200

    (front-end) -skill stste model

--- team-utilisation-monitor/libs/client/individual/feature/src/lib/state/skill.state.ts
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/state/skill.state.ts
@@ -1,10 +1,6 @@
 import{ State, Action, StateContext, Selector} from '@ngxs/store';
 import {Skill} from './../models/skill.model';
 import{ AddSkill} from "../actions/skill.actions";
-import {
-  IncreaseNumberOfEmployees
-} from "../../../../../admin/feature/src/lib/actions/mutate-number-of-employees.action";
-import {IncreaseNumberOfEmployeesStateModel} from "@team-utilisation-monitor/client/admin/feature";
 
 export class SkillStateModel{
   skill: Skill[]=[]
@@ -24,11 +20,11 @@ export class SkillState{
       return state.skill[state.skill.length-1].name;
   }
 
-  @Action(skill)
-  increase({getState,patchState}:StateContext<SkillStateModel>,{payload}:skill){
+  @Action(AddSkill)
+  increase({getState,patchState}:StateContext<SkillStateModel>,{skill}:AddSkill){
     const state=getState(); //gets the current state for us.
     patchState({
-      skill:[...state.skill,payload] //append the new state to the employees
+      skill:[...state.skill,skill] //append the new state to the employees
     })
   }
 
