3493f804b90c73b0151dda07af0df6a6e9f1cecb
thangeniF <faresatheo1@gmail.com>
2022-08-20 21:30:32 +0200

    🚧(front-end)-individual skill constructor

---
+++ team-utilisation-monitor/libs/client/individual/feature/src/lib/state/skill.state.ts
@@ -0,0 +1,7 @@
+import{ State, Action, StateContext, Selector} from '@ngxs/store';
+import {Skill} from './../models/skill.model';
+import{ AddSkill} from "../actions/skill.actions";
+
+export class SkillStateModel{
+  skill: Skill
+}
