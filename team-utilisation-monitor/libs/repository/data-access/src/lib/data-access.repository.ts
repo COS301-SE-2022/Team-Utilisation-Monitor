ff05a9b1c4c2fd15f01c28d44f277fd3293c2a55
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-09-06 01:14:27 +0200

    AssignHours Fixed

--- team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.ts
+++ team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.ts
@@ -2463,7 +2463,16 @@ export class DataAccessRepository {
 
             let AssignedHours=Math.round((PersonObj.assigned_hours+(await this.HoursPerTeamMemberOnProject(TeamsOnProject[i].team_id,projectId)))*100)/100;
             let WeeklyHours=PersonObj.weekly_hours;
-            let Utilization=Math.round(((AssignedHours/WeeklyHours)*100)*100)/100;
+            let Utilization=0;
+
+            if(AssignedHours>0)
+            {
+              Utilization=Math.round(((AssignedHours/WeeklyHours)*100)*100)/100;
+            }
+            else
+            {
+              Utilization=0;
+            }
 
             let Statuss:Status
 
@@ -2559,7 +2568,16 @@ export class DataAccessRepository {
 
             let AssignedHours=Math.round((PersonObj.assigned_hours+(await this.HoursPerTeamMemberOnProject(TeamsOnProject[i].team_id,projectId)))*100)/100;
             let WeeklyHours=PersonObj.weekly_hours;
-            let Utilization=Math.round(((AssignedHours/WeeklyHours)*100)*100)/100;
+            let Utilization=0;
+
+            if(AssignedHours>0)
+            {
+              Utilization=Math.round(((AssignedHours/WeeklyHours)*100)*100)/100;
+            }
+            else
+            {
+              Utilization=0;
+            }
 
             let Statuss:Status
 
@@ -2656,7 +2674,17 @@ export class DataAccessRepository {
 
             let AssignedHours=Math.round((PersonObj.assigned_hours-(await this.HoursPerTeamMemberOnProject(TeamsOnProject[i].team_id,projectId)))*100)/100;
             let WeeklyHours=PersonObj.weekly_hours;
-            let Utilization=Math.round(((AssignedHours/WeeklyHours)*100)*100)/100;
+
+            let Utilization=0;
+
+            if(AssignedHours>0)
+            {
+              Utilization=Math.round(((AssignedHours/WeeklyHours)*100)*100)/100;
+            }
+            else
+            {
+              Utilization=0;
+            }
 
             let Statuss:Status
 
@@ -3266,14 +3294,29 @@ export class DataAccessRepository {
 
         if(p_id>0){
 
+            const person=await this.prisma.person.findUnique({
+            where:{
+                id:p_id,
+            }
+            })
 
+            let Utilization=0;
+            if(hours>0)
+            {
+              Utilization=person.assigned_hours/hours;
+            }
+            else
+            {
+              Utilization=0;
+            }
 
             await this.prisma.person.update({
                 where:{
                     id:p_id,
                 },
                 data:{
-                    weekly_hours:hours
+                    weekly_hours:hours,
+                    utilisation:Utilization
                 }
             })
 
