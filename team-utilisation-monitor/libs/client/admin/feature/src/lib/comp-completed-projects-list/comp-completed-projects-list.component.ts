232b041be54109e0f18f5320b5f82cb557565a82
Cornel Coetzee <u20586737@tuks.co.za>
2022-09-22 12:09:39 +0200

    Create Completed Project Component
    - Created component for the completed Project list

---
+++ team-utilisation-monitor/libs/client/admin/feature/src/lib/comp-completed-projects-list/comp-completed-projects-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'team-utilisation-monitor-comp-completed-projects-list',
+  templateUrl: './comp-completed-projects-list.component.html',
+  styleUrls: ['./comp-completed-projects-list.component.scss']
+})
+export class CompCompletedProjectsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
