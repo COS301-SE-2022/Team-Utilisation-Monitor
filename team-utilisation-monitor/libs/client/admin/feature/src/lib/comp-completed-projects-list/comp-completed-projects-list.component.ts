41a051fbc6a7ad32e46c3f8506360130a38b986c
Cornel Coetzee <u20586737@tuks.co.za>
2022-09-22 12:15:12 +0200

    Completed the formatting of the CompCompletedProjects component
    - added html
    - added scss
    - added TS linking variables

--- team-utilisation-monitor/libs/client/admin/feature/src/lib/comp-completed-projects-list/comp-completed-projects-list.component.ts
+++ team-utilisation-monitor/libs/client/admin/feature/src/lib/comp-completed-projects-list/comp-completed-projects-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'team-utilisation-monitor-comp-completed-projects-list',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CompCompletedProjectsListComponent implements OnInit {
 
   constructor() { }
+
+  @Input() Projects!:{projectName:string, manHours:number};
 
   ngOnInit(): void {
   }
