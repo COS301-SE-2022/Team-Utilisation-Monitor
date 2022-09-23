be5721a9328e69aeaca65468c573fa31a9b6e496
thangeniF <faresatheo1@gmail.com>
2022-09-23 19:36:37 +0200

    🚧 (front-end)- object description and first field

---
+++ team-utilisation-monitor/libs/api/shared/data-access/src/lib/api-trend-skill.entity.ts
@@ -0,0 +1,16 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { ErrorStrings } from "@team-utilisation-monitor/shared/services/prisma-services";
+
+@ObjectType({description:'Object encapsulating the recommended skill details'})
+export class UserPerson{
+
+    @Field(() => ID)
+    id?:number;
+
+   
+
+    @Field(()=>ErrorStrings,{defaultValue:ErrorStrings.NONE})
+    error_string?:ErrorStrings;
+
+
+}
