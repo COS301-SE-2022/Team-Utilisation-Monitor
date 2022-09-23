b647ffaadea94e9017544fa3decfa0357fe846b3
Rourke9001 <92760271+Rourke9001@users.noreply.github.com>
2022-09-23 02:51:51 +0200

    Added unit test for returnCompanyObject

--- team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.spec.ts
+++ team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.spec.ts
@@ -3,6 +3,7 @@ import { DataAccessRepository } from './data-access.repository';
 
 import { PrismaService } from '@team-utilisation-monitor/shared/services/prisma-services';
 
+import { UserPerson, UserCompany, InviteCodeEntity, CompanyStatsEntity ,Skill, UserStatsEntity, CompanyUtilization, PositionEntity} from '@team-utilisation-monitor/api/shared/data-access'
 
 
 describe('DataAccessRepository', () => {
@@ -33,17 +34,72 @@ describe('DataAccessRepository', () => {
     const company_name = "UP";
     const company_id = 123;
 
-    it('should return a user object', () => {
+    it('should return a user object', async () => {
       try {
         const user = repository.returnObject(id, name, surname, email, suspended, role, company_name, position, company_id);
         expect(user).toEqual(user);
+        expect(user).toHaveReturned;
       } catch (error) { 
         fail(error)
       }
     })
   }); 
 
-  // to do
+  describe('@returnCompanyObject', () => {
+
+    const all_users = [];
+
+    let user_person = new UserPerson();
+
+    const position = new PositionEntity();
+
+    position[0] = "admin"
+
+    position[1] = "tester"
+
+    user_person.id = 1;
+    user_person.name = "Rourke";
+    user_person.surname = "Amiss";
+    user_person.email= "test@example.com";
+    user_person.role = "tester";
+    user_person.suspended = false;
+    user_person.positions = position[0];
+    user_person.company_name = "UP";
+    user_person.company_id = 123;
+
+    all_users[0] = user_person;
+
+    user_person = new UserPerson();
+
+    user_person.id = 66;
+    user_person.name = "Sam";
+    user_person.surname = "Smith";
+    user_person.email = "ssmith@gmail.com";
+    user_person.role = "developer";
+    user_person.suspended = false;
+    user_person.positions = position[1];
+    user_person.company_name = "UP";
+    user_person.company_id = 123;
+
+    all_users[1] = user_person;
+        
+    const id = 1;
+    const company_name = "Grand Tour";
+    const admins = all_users[0];
+    const employees = all_users[1];
+    const projects = "tester";
+    const teams = false;
+    const invite_code = "intern";
+
+    it('should return a company object', () => {
+      try {
+        const company = repository.returnCompanyObject(id, company_name, admins, employees, projects, teams, invite_code);
+        expect(company).toEqual(company);
+      } catch (error) { 
+        fail(error)
+      }
+    })
+  }); 
 
   
 });
--- team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.ts
+++ team-utilisation-monitor/libs/repository/data-access/src/lib/data-access.repository.ts
@@ -17,7 +17,7 @@ export class DataAccessRepository {
 
     async returnObject(id:number,name:string,surname:string,email:string,suspended:boolean,role:string,company:string,position:string,company_id:number)
     {
-        const user_person=new UserPerson();
+        const user_person = new UserPerson();
 
         user_person.id=id;
         user_person.name=name;
@@ -28,7 +28,7 @@ export class DataAccessRepository {
         user_person.company_name=company;
         user_person.company_id=company_id;
 
-        
+
         return user_person;
 
     }
